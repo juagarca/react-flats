a259e84e0c611949ae79a2de81124f874b5f633d
Juan Garcia <juagarca@gmail.com>
2020-12-23 13:37:03 +0000

    added link to flat card

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Flat extends Component {
   constructor(props) {
     super(props);
-    console.log("flat created");
+    // console.log("flat created");
   }
 
   render() {
@@ -18,6 +18,7 @@ class Flat extends Component {
       >
         <div className="card-category">{this.props.price} {this.props.currency}</div>
         <div className="card-description"><h2>{this.props.name}</h2></div>
+        <a className="card-link" href="#"></a>
       </div>
     );
   }
